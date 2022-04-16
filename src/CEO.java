2d186f16de91460673937673bb89683ce4b09728
gsiguenza12 <gebsiguenza@gmail.com>
2022-04-16 12:55:53 -0700

    Changed local version to run program with leak configured as main

--- src/CEO.java
+++ src/CEO.java
@@ -1,5 +1,7 @@
 public class CEO extends Administrator {
+
     public CEO(String name) {
         super(name);
     }
+
 }
--- src/leak.java
+++ src/leak.java
@@ -1,3 +1,10 @@
+/**
+ * Demo class demonstrating hierarchical chain of responsibility.
+ *
+ * You want an object to be able to send a command to another object without specifying the receiver. The sending object does not care
+ * which object handles the command, only that an object will receive a command and handle it.
+ *
+ */
 public class leak
 {
     public static void main(String[] args) {
